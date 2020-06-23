b27db030f8d6fd160279941add166cc1da855014
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-06-23 22:10:33 +0300

    Lab_19_1

---
+++ Lab_19_1/Lab_19_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+//Scrieti un iterator care sa furnizeze toate numerele de la 0 pana la numarul maxim reprezentabil in memorie pentru 
+//tipurile intregi.Folositi yield return si yield break pentru a obtine comportamentul dorit.
+
+namespace Lab_19_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (long number in IntegerNumbers.IntIterator(35))
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        public static class IntegerNumbers
+        {
+
+            public static IEnumerable<long> IntIterator(int n)
+            {
+                yield return 0;
+                int N = 0;
+
+                for (int i = 1; i < int.MaxValue; i++)
+                {
+                    N++;
+                    if (i == n)
+                  
+                        {
+                            yield break;
+                        }
+
+                    
+                    yield return N;
+
+                }
+            }
+        }
+    }
+}
