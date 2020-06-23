766d47fb074eda7d87df46af0ab024db3cf31445
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-06-23 23:15:19 +0300

    Lab_19_1

--- Lab_19_1/Lab_19_1/Program.cs
+++ Lab_19_1/Lab_19_1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 
 
@@ -10,9 +11,30 @@ namespace Lab_19_1
     {
         static void Main(string[] args)
         {
-            foreach (long number in IntegerNumbers.IntIterator(35))
+            //Console.WriteLine($"Generare numere intregi de la 0 la 35 :");
+            Console.WriteLine("Numarul intreg maxim dorit : ");
+
+            bool r = int.TryParse(Console.ReadLine(), out int n);
+            if (r)
             {
-                Console.WriteLine(number);
+                foreach (long number in IntegerNumbers.IntIterator(n))
+                {
+                    Console.WriteLine(number);
+                }
+
+                Console.WriteLine();
+
+
+                Console.WriteLine($"Generare numere impare de la 0 la n :");
+                var oddNumbers = new OddNumbersEnumerable(IntegerNumbers.IntIterator(n));
+                foreach (int number in oddNumbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus nu este intreg  {n} ");
             }
         }
 
@@ -27,19 +49,52 @@ namespace Lab_19_1
                 for (int i = 1; i < int.MaxValue; i++)
                 {
                     N++;
-                    if (i == n)
-                     
+
+                    if (i == n+1)                     
                   
                         {
-                        Console.WriteLine($"S-au generat numerele intregi de la 0 la {n}");
+                        //Console.WriteLine($"S-au generat numerele intregi de la 0 la {n}");
+
                             yield break;
                         }
-
                     
                     yield return N;
-
                 }
             }
         }
+
+
+        public class OddNumbersEnumerable : IEnumerable<long>
+        {
+            private readonly IEnumerable<long> elements;
+
+            public OddNumbersEnumerable(IEnumerable<long> elements)
+            {
+                this.elements = elements ?? Enumerable.Empty<long>();
+            }
+
+            public IEnumerator<long> GetEnumerator()
+            {
+                return this.GetOddNumbersEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return this.GetOddNumbersEnumerator();
+            }
+
+            protected IEnumerator<long> GetOddNumbersEnumerator()
+            {
+                foreach (var number in this.elements)
+                {
+                    if  (number % 2 == 1)
+                    {
+                        yield return number;
+                    }
+                }
+            }
+        }
+
+        
     }
 }
